bca17d3c3717b2677ff3b9d08678f79c49859fe4
sc-aug <shichuanwang@yahoo.com>
2017-12-23 18:51:14 -0600

    hw

---
+++ cis193/hw5/hw5.go
@@ -0,0 +1,79 @@
+// Homework 5: Goroutines
+// Due March 3, 2017 at 11:59pm
+package main
+
+import "fmt"
+
+func main() {
+	// Feel free to use the main function for testing your functions
+	hello := make(chan string, 5)
+	hello <- "Hello world"
+	hello <- "Привет мир"
+	hello <- "Привіт Світ"
+	hello <- "Witaj świecie"
+	close(hello)
+	fmt.Println(<-hello)
+	for greeting := range hello {
+		fmt.Println(greeting)
+	}
+}
+
+// Filter copies values from the input channel into an output channel that match the filter function p
+// The function p determines whether an int from the input channel c is sent on the output channel
+func Filter(c <-chan int, p func(int) bool) <-chan int {
+	// TODO
+	return nil
+}
+
+// Result is a type representing a single result with its index from a slice
+type Result struct {
+	index  int
+	result string
+}
+
+// ConcurrentRetry runs all the tasks concurrently and sends the output in a Result channel
+//
+// concurrent is the limit on the number of tasks running in parallel. Your
+// solution must not run more than `concurrent` number of tasks in parallel.
+//
+// retry is the number of times that the task should be attempted. If a task
+// returns an error, the function should be retried immediately up to `retry`
+// times. Only send the results of a task into the output channel if it does not error.
+//
+// Multiple instances of ConcurrentRetry should be able to run simultaneously
+// without interfering with one another, so global variables should not be used.
+// The function must return the channel without waiting for the tasks to
+// execute, and all results should be sent on the output channel. Once all tasks
+// have been completed, close the channel.
+func ConcurrentRetry(tasks []func() (string, error), concurrent int, retry int) <-chan Result {
+	// TODO
+	return nil
+}
+
+// Task is an interface for types that process integers
+type Task interface {
+	Execute(int) (int, error)
+}
+
+// Fastest returns the result of the fastest running task
+// Fastest accepts any number of Task structs. If no tasks are submitted to
+// Fastest(), it should return an error.
+// You should return the result of a Task even if it errors.
+// Do not leave any pending goroutines. Make sure all goroutines are cleaned up
+// properly and any synchronizing mechanisms closed.
+func Fastest(input int, tasks ...Task) (int, error) {
+	// TODO
+	return 0, nil
+}
+
+// MapReduce takes any number of tasks, and feeds their results through reduce
+// If no tasks are supplied, return an error.
+// If any of the tasks error during their execution, return an error immediately.
+// Once all tasks have completed successfully, return the value of reduce on
+// their results in any order.
+// Do not leave any pending goroutines. Make sure all goroutines are cleaned up
+// properly and any synchronizing mechanisms closed.
+func MapReduce(input int, reduce func(results []int) int, tasks ...Task) (int, error) {
+	// TODO
+	return 0, nil
+}
---
+++ cis193/hw7/hw7.go
@@ -0,0 +1,96 @@
+// Homework 7: Web Scraping
+// Due March 28, 2017 at 11:59pm
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	doc, err := goquery.NewDocument("http://uszip.com/zip/19104")
+	if err != nil {
+		log.Fatal(err)
+	}
+	city := doc.Find("div.zip-data hgroup h2 strong").Eq(0).Text()
+	fmt.Println(city)
+}
+
+// News is a Hacker News article listing
+type News struct {
+	Points   int
+	Title    string
+	Username string
+	URL      string
+}
+
+// NewsSlice is a slice of News pointers
+type NewsSlice []*News
+
+// ScrapeHackerNews scrapes the website "https://news.ycombinator.com/" using goquery and returns
+// information on the first n posts.
+//
+// For each post, the attributes to be extracted are: points, title, username and url.
+// This data should be returned as a NewsSlice, where NewsSlice is a custom slice of News structs.
+//
+// For example, for the sample image located at `https://www.cis.upenn.edu/~cis193/homeworks/hn.png`,
+// the struct would look like:
+// News{24, "QEMU(TCG): user-to-root privesc inside VM via bad translation caching",
+// "webaholic", "https://bugs.chromium.org/p/project-zero/issues/detail?id=1122"}.
+//
+// If n is greater than the number of total posts available (which should be 30), return data from
+// the all of the available posts (all thirty).
+func ScrapeHackerNews(n int) NewsSlice {
+	// TODO
+	return nil
+}
+
+// GetEmails returns a string slice of the emails found on the given URL.
+//
+// Scenario: you are a student enthusiastic about spreading awareness about Go. To effectively
+// market Go, you decide to email Penn CIS professors about the wonders of the Go programming
+// language. In this function, use goquery to extract the email addresses from the URL
+// "http://www.cis.upenn.edu/about-people/" and return them as a string slice. This will involve you
+// having to investigate where and how emails are located on the webpage.
+// Note: you should have 47 total emails returned.
+func GetEmails() []string {
+	// TODO
+	return nil
+}
+
+// CountryData has GDP information on a country
+type CountryData struct {
+	Country string
+	GDP     string
+}
+
+// GetCountryGDP takes in a string country name and returns the GDP (in millions) as
+// an integer. Information on the country is found by concurrently scraping a hidden website with
+// data on countries scattered on many pages.
+//
+// Scenario: imagine you are a spy and you have discovered a URL with top secret GDP information:
+// "https://www.cis.upenn.edu/~cis193/scraping/9828772efc2bd314a277c8880695dea2.html". This webpage
+// has a country name and the GDP (in millions of US Dollars). It also has links to two other
+// country's webpages. Based on intelligence you've received, every country has a webpage on this
+// website with information about it, but you do not know the URL for each page. You can assume that
+// none of the page links lead you to a cycle and every country can be reached from a path from the
+// initial URL that you are given. So, for this function, you will need to traverse from the initial
+// url to every webpage link you encounter in order to find information on the target `country`
+// string. Since time is of the essence, you want to use concurrency to scrape webpages
+// simultaneously. Note that for this function, we only care about getting the GDP for the input
+// `country` string. You may find it useful to use the CountryData struct to send country
+// information between goroutines.
+//
+// To prevent the function from getting stuck if an invalid `country` string is entered,
+// you should also implement a timeout that will automatically return an error after 10 seconds
+// if the program hasn't already finished terminating.
+//
+// Feel free to make and use helper functions for this function. To help with testing this
+// function, we know from intelligence reports that the GDP for "Canada" is 1532343 and
+// the GDP for "Colombia" is 274135.
+func GetCountryGDP(country string) (int, error) {
+	// TODO
+	return 0, nil
+}
